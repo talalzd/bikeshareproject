63dd55a3adae3ba7c8d672571971c12e6fa330a3
Talal Alzayed <talal.h.zd@gmail.com>
2021-03-22 19:15:22 +0300

    Fixed the requirements for the project

--- python.py
+++ python.py
@@ -213,6 +213,14 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+    x = 1
+    while True:
+        raw = input('\nWould you like to see some raw data? Enter yes or no.\n')
+        if raw.lower() == 'yes':
+            print(df[x:x+5])
+            x = x+5
+        else:
+            break
 
 def main():
     while True:
